3cb5e3884787689080fe95d95e1e2546752d2752
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-29 18:30:07 -0500

    Adding a bit of fun stuff

--- src/todos/mocks/index.ts
+++ src/todos/mocks/index.ts
@@ -6,17 +6,17 @@ export const mockTodos = [
   },
   {
     id: '2',
-    title: 'todo 2',
+    title: 'poner a dormir a valeria',
     completed: false
   },
   {
     id: '3',
-    title: 'todo 3',
+    title: 'casarme',
     completed: false
   },
   {
     id: '4',
-    title: 'todo 4',
+    title: 'comprar hot wings',
     completed: false
   }
 ]
