b86b43965714fb02893e889f8319928489e31777
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-29 17:59:36 -0500

    Fix null error in `main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import 'todomvc-app-css/index.css'
 import './styles/index.css'
 
 /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion  */
-ReactDOM.createRoot(document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root') as Element)
   .render(
   <React.StrictMode>
     <App />
